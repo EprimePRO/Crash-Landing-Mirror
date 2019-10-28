0015524b2eb9b80421c102b7a3e5fced26e0d17e
EprimePRO <emanuel_rios9@yahoo.com>
2019-10-28 00:43:17 -0500

    changed the way the camera follows the player and how the player is instantiated

--- Assets/Scripts/Level/CameraControl.cs
+++ Assets/Scripts/Level/CameraControl.cs
@@ -17,7 +17,6 @@ public class CameraControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
         offset = transform.position - player.transform.position;
     }
 

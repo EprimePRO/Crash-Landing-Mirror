510d26bace510a066fa7e4c24b4024c4f5e2a9f6
EprimePRO <emanuel_rios9@yahoo.com>
2019-10-27 22:59:26 -0500

    added camera zoom

---
+++ Assets/Scripts/Level/CameraControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Camera camera;
+
+    float cameraDistanceMax = 5f;
+    float cameraDistanceMin = 2f;
+    float cameraDistance = 5f;
+    float scrollSpeed = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraDistance += Input.GetAxis("Mouse ScrollWheel") * scrollSpeed * -1 * Time.fixedDeltaTime;
+        cameraDistance = Mathf.Clamp(cameraDistance, cameraDistanceMin, cameraDistanceMax);
+
+        camera.orthographicSize = cameraDistance;
+    }
+}

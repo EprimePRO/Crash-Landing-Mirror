f7a0854d37fab8e6c9b9b9c7f977915ca4ff9b37
EprimePRO <emanuel_rios9@yahoo.com>
2019-11-10 11:07:21 -0600

    changed GameManager to a singleton class

--- Assets/Scripts/Level/GameManager.cs
+++ Assets/Scripts/Level/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance = null;
 
     public GameObject[] enemiesPrefabs;
     public GameObject playerPrefab = null;
@@ -17,6 +18,24 @@ public class GameManager : MonoBehaviour
     public Text resourceText;
     public int resources = 0;
     private Turret chosenTurret;
+
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+    }
 
     // Start is called before the first frame update
     void Start()
